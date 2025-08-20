fec02fd9214c6f0efdbdbcf4b590495b6645bf2a
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2025-08-20 13:09:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",   // listen on all interfaces
+    port: 5173,        // ensure port is fixed
+    strictPort: true,  // fail instead of switching to another port
+  },
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
